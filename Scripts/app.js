eafde02ac033f457e7ddfe0fa0bef8a772cc85f6
frontendnath <nathanjoe.dev@gmail.com>
2023-05-15 23:36:34 +0100

    code update

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,16 +3,72 @@
 const addTodoButtonElement = document.getElementById('addTodoButton');
 const todoNameElement = document.getElementById('todoName');
 
-const addedTodo = [];
+const todoData = [];
+
+renderTodoItem();
+
+function renderTodoItem() {
+
+  let displayInHtml = '';
+
+  for (let i = 0; i < todoData.length; i++) {
+
+    const todoValue = todoData[i];
+    const addedTodoToDOM = `
+
+    <div class="bg-light mt-2 d-flex w-100 border p-3 align-items-center">
+      <div class="me-auto">
+        <b class="text-capitalize">
+          ${todoValue}
+        </b>
+      </div>
+
+      <div>
+        <button onclick="todoData.splice(${i}, 1); renderTodoItem()" class="bg-transparent me-2 border-0 text-danger">
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>
+    </div>
+
+    `;
+    displayInHtml += addedTodoToDOM;
+
+  }
+
+  const addedTodoContainerElement = document.getElementById('addedTodoContainer');
+
+  if(todoData.length === 0) {
+
+    addedTodoContainerElement.innerHTML = `
+    
+      <div class="d-flex text-muted text-center align-items-center justify-content-center" style="height: 150px;">
+
+        <div>
+          <h1 class="fw-bolder">0</h1>
+          <h5>Todo added</h5>
+        </div>
+
+      </div>
+
+    `;
+
+  } else {
+
+    addedTodoContainerElement.innerHTML = displayInHtml;
+
+  }
+
+}
+
 
 addTodoButtonElement.addEventListener('click', () => {
 
   const todoNameValue = todoNameElement.value;
 
-  addedTodo.push(todoNameValue);
+  todoData.push(todoNameValue);
 
-  console.log(addedTodo);
+  todoNameElement.value = '';
 
-  addTodoButtonElement.value = '';
+  renderTodoItem();
 
 });
