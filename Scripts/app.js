f7696a2b44ca6e3dab967b8b865083ae9cbbac1a
frontendnath <nathanjoe.dev@gmail.com>
2023-05-16 12:06:51 +0100

    code update

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,7 @@
 
 const addTodoButtonElement = document.getElementById('addTodoButton');
 const todoNameElement = document.getElementById('todoName');
+const todoDueDataElement = document.getElementById('dueData');
 
 const todoData = [];
 
@@ -14,20 +15,53 @@ function renderTodoItem() {
   for (let i = 0; i < todoData.length; i++) {
 
     const todoValue = todoData[i];
+
+    const { taskName, dueData } = todoValue;
+
     const addedTodoToDOM = `
 
-    <div class="bg-light mt-2 d-flex w-100 border p-3 align-items-center">
-      <div class="me-auto">
-        <b class="text-capitalize">
-          ${todoValue}
-        </b>
+    <div class="todo_block">
+      <div class="bg-light task_box mt-2 d-flex w-100 border p-3 align-items-center">
+        <div class="me-auto">
+          <div>
+            <b class="text-capitalize">
+              ${taskName}
+            </b>
+          </div>
+
+          <div class="text-muted">
+            ${dueData}
+          </div>
+        </div>
+
+        <div>
+          <button title="delete" onclick="
+
+          swal({
+            title: 'Are you sure?',
+            text: 'Once deleted, you will not be able to recover this imaginary file!',
+            icon: 'warning',
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal('Poof! Todo item has been deleted!', {
+                icon: 'success',
+              });
+
+              todoData.splice(${i}, 1); renderTodoItem()
+            } else {
+              swal('Todo app not deleted!');
+            }
+          });
+
+          " class="bg-transparent border-0 text-danger">
+            <i class="fas fa-trash"></i>
+          </button>
+        </div>
       </div>
 
-      <div>
-        <button onclick="todoData.splice(${i}, 1); renderTodoItem()" class="bg-transparent me-2 border-0 text-danger">
-          <i class="fas fa-trash"></i>
-        </button>
-      </div>
     </div>
 
     `;
@@ -45,7 +79,7 @@ function renderTodoItem() {
 
         <div>
           <h1 class="fw-bolder">0</h1>
-          <h5>Todo added</h5>
+          <h5>Task added</h5>
         </div>
 
       </div>
@@ -64,11 +98,28 @@ function renderTodoItem() {
 addTodoButtonElement.addEventListener('click', () => {
 
   const todoNameValue = todoNameElement.value;
+  const todoDateValue = todoDueDataElement.value;
 
-  todoData.push(todoNameValue);
+  if(todoNameValue.length === 0 || todoDateValue.length === 0) {
 
-  todoNameElement.value = '';
+    swal("Error!", "Task name or date cannot be empty!", "error");
 
-  renderTodoItem();
+  } else {
+
+    swal("Success!", "Task has been added!", "success");
+
+    todoData.push({
+
+      taskName: todoNameValue,
+      dueData: todoDateValue
+  
+    });
+
+    todoNameElement.value = '';
+    todoDueDataElement.value = '';
+
+    renderTodoItem();
+
+  }
 
 });
